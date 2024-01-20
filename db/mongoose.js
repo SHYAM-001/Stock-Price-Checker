8b41f5350c42b226a72fb240ea9f33fffaf8e7b7
SHYAM-001 <shyamkathir007@gmail.com>
2024-01-20 22:48:15 +0530

    made some changes in the database

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 require("dotenv").config();
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = "mongodb+srv://shyamkathir:Shyamsharmi007@cluster0.byojbkp.mongodb.net/freecodecamp?retryWrites=true&w=majority"; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 mongoose.connect(CONNECTION_STRING, {
   useNewUrlParser: true,
